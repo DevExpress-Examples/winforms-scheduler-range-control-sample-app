e10ddf4d98872cd4f4b9ef8ac3e51a3894c47c24
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SampleSchedulerRangeControl/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+using DevExpress.XtraBars.Ribbon;
+using DevExpress.XtraBars.Helpers;
+using DevExpress.Skins;
+using DevExpress.LookAndFeel;
+using DevExpress.UserSkins;
+
+
+namespace SampleSchedulerRangeControl
+{
+    public partial class Form1 : RibbonForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitSkinGallery();
+            schedulerControl.Start = System.DateTime.Now;
+
+            schedulerStorage.AppointmentsChanged += new DevExpress.XtraScheduler.PersistentObjectsEventHandler(schedulerStorage_AppointmentsChanged);
+            schedulerStorage.AppointmentsInserted += new DevExpress.XtraScheduler.PersistentObjectsEventHandler(schedulerStorage_AppointmentsInserted);
+            schedulerStorage.AppointmentsDeleted += new DevExpress.XtraScheduler.PersistentObjectsEventHandler(schedulerStorage_AppointmentsDeleted);
+
+            schedulerStorage.Appointments.CommitIdToDataSource = false;
+        }
+        void InitSkinGallery()
+        {
+            SkinHelper.InitSkinGallery(rgbiSkins, true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'carsDBDataSet.Cars' table. You can move, or remove it, as needed.
+            this.carsTableAdapter.Fill(this.carsDBDataSet.Cars);
+            // TODO: This line of code loads data into the 'carsDBDataSet.CarScheduling' table. You can move, or remove it, as needed.
+            this.carSchedulingTableAdapter.Fill(this.carsDBDataSet.CarScheduling);
+
+        }
+
+        void schedulerStorage_AppointmentsDeleted(object sender, DevExpress.XtraScheduler.PersistentObjectsEventArgs e)
+        {
+            Commit();
+        }
+
+        void schedulerStorage_AppointmentsInserted(object sender, DevExpress.XtraScheduler.PersistentObjectsEventArgs e)
+        {
+            Commit();
+        }
+
+        void schedulerStorage_AppointmentsChanged(object sender, DevExpress.XtraScheduler.PersistentObjectsEventArgs e)
+        {
+            Commit();
+        }
+
+        private void Commit()
+        {
+            carSchedulingTableAdapter.Update(carsDBDataSet);
+            carsDBDataSet.AcceptChanges();
+        }
+
+    }
+}
---
+++ VB/SampleSchedulerRangeControl_VB/Form1.vb
@@ -0,0 +1,49 @@
+﻿Imports System.ComponentModel
+Imports DevExpress.XtraBars
+Imports DevExpress.XtraBars.Ribbon
+Imports DevExpress.XtraBars.Helpers
+Imports DevExpress.Skins
+Imports DevExpress.LookAndFeel
+Imports DevExpress.UserSkins
+
+
+Public Class Form1
+    Sub New()
+        InitializeComponent()
+        Me.InitSkinGallery()
+        schedulerControl.Start = DateTime.Now
+        AddHandler schedulerStorage.AppointmentsChanged, AddressOf schedulerStorage_AppointmentsChanged
+        AddHandler schedulerStorage.AppointmentsInserted, AddressOf schedulerStorage_AppointmentsInserted
+        AddHandler schedulerStorage.AppointmentsDeleted, AddressOf schedulerStorage_AppointmentsDeleted
+        schedulerStorage.Appointments.CommitIdToDataSource = False
+    End Sub
+
+    Private Sub InitSkinGallery()
+        SkinHelper.InitSkinGallery(rgbiSkins, True)
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'CarsDBDataSet.Cars' table. You can move, or remove it, as needed.
+        Me.CarsTableAdapter.Fill(Me.CarsDBDataSet.Cars)
+        'TODO: This line of code loads data into the 'CarsDBDataSet.CarScheduling' table. You can move, or remove it, as needed.
+        Me.CarSchedulingTableAdapter.Fill(Me.CarsDBDataSet.CarScheduling)
+
+    End Sub
+
+    Private Sub schedulerStorage_AppointmentsDeleted(ByVal sender As Object, ByVal e As DevExpress.XtraScheduler.PersistentObjectsEventArgs)
+        Commit()
+    End Sub
+
+    Private Sub schedulerStorage_AppointmentsInserted(ByVal sender As Object, ByVal e As DevExpress.XtraScheduler.PersistentObjectsEventArgs)
+        Commit()
+    End Sub
+
+    Private Sub schedulerStorage_AppointmentsChanged(ByVal sender As Object, ByVal e As DevExpress.XtraScheduler.PersistentObjectsEventArgs)
+        Commit()
+    End Sub
+
+    Private Sub Commit()
+        carSchedulingTableAdapter.Update(carsDBDataSet)
+        carsDBDataSet.AcceptChanges()
+    End Sub
+End Class
